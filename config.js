70fb0ba93078b669d425635bc33001bd1146345b
Mike Castillo <mike.castillo@insight.com>
2022-02-18 17:27:41 -0600

    Initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports.oauthConfig = {
+    Google: {
+        clientID    : "991408914015-8ip6fvp4ipihuu9ce6k3dpudmept1k3a.apps.googleusercontent.com",
+        clientSecret   : "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"
+    }
+}
